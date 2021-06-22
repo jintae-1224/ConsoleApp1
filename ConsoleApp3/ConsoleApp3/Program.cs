d556204598c8da2452a9290332b27675ff809d7d
jinatae <teaminnoper@gmail.com>
2021-06-22 15:25:32 +0900

    c# class 생성

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Calculator.Plus(3, 4);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+    class Calculator
+    {
+        public static int Plus (int a, int b)
+        {
+            Console.WriteLine("Input : {0}, {1}", a, b);
+            int result = a + b;
+            return result;
+        }
+    }
+}
