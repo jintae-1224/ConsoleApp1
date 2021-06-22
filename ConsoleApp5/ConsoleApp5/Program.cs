53d58c2bca8994e9525fca30adac7ca85c4fac1c
jinatae <teaminnoper@gmail.com>
2021-06-22 16:26:35 +0900

    c# interface

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    interface ILogger
+    {
+        void WriteLog(string log);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new ConsoleLogger();
+            logger.WriteLog("SomeLog");
+        }
+    }
+}
