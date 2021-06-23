b62746c1deb86f18a1f9ad01204e4ec7e4698265
jinatae <teaminnoper@gmail.com>
2021-06-23 16:48:40 +0900

    wpf study1

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    class HandleAnEvent
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+
+            Window win = new Window();
+            win.Title = "Handle An Event";
+            win.MouseDown += WindowOnMouseDown;
+
+            app.Run(win);
+        }
+        static void WindowOnMouseDown(object sender, MouseButtonEventArgs args)
+        {
+            Window win = sender as Window;
+            string strMessage = string.Format("Window Clicked with {0} button at point ({1})", args.ChangedButton, args.GetPosition(win));
+            MessageBox.Show(strMessage, win.Title);
+        }
+    }
+    }
---
+++ WpfApp3/WpfApp5/ShowMyFace.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp5
+{
+    class ShowMyFace : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new ShowMyFace());
+        }
+
+        public ShowMyFace()
+        {
+            Title = "Show My Face";
+
+            Uri uri = new Uri("https://www.charlespetzold.com/PetzoldTattoo.jpg");
+            BitmapImage bitmap = new BitmapImage(uri);
+
+            Image img = new Image();
+            img.Source = bitmap;
+            Content = img;
+        }
+    }
+}
